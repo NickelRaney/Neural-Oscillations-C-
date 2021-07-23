49f4269c2f1419a1241a03c7822aea8a42ca9472
NickelRaney <raneywu@outlook.com>
2021-07-23 11:56:20 +0800

    update

--- numerical_test_of_pending_spike.m
+++ numerical_test_of_pending_spike.m
@@ -6,8 +6,8 @@ for t=0.05:0.05:10
     var_spike(round(t/0.05))=var(spike_t<t);
 end
 
-var_spike(41:end)=var_spike(41:end)+0.5*var_spike(1:161);
-plot(var_spike*400);
+% var_spike(41:end)=var_spike(41:end)+0.5*var_spike(1:161);
+plot(var_spike);
 hold on
 %%
 plot(var_spike2);
